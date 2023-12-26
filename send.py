0970600cf8911f34b4a7778c910d9ae6e0995c58
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-26 22:32:25 +0100

    chore: gives sending progress

--- send.py
+++ send.py
@@ -2,10 +2,13 @@
 from models.sender import send_email
 from resources import participants, subject, body
 
+print("Sending emails...")
 
-for participant in participants:
-    personalized_body = body.replace("[Participant's Name]", participant["name"].split()[1])
+for index, participant in participants.iterrows():
+    personalized_body = body.replace("[Participant's Name]", participant["Name"].split()[1])
 
-    send_email(participant["email"], subject, personalized_body)
+    print(f"Sending email to {participant['Email']}")
+    send_email(participant["Email"], subject, personalized_body)
+    print(f'Email sent to {participant["Email"]}')
 
-print("Email sent successfully.")
+print("All emails sent successfully")
