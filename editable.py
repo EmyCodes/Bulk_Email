1454301eb9a69aa4c9b3c96c8da260f354888218
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-26 22:34:47 +0100

    chore: define smtp_server

--- editable.py
+++ editable.py
@@ -2,6 +2,10 @@
 
 smtp_server = "smtp.gmail.com"  # This is for Gmail, It can be changed to any other SMTP server
 
-sender_name = "Sender's Name"   # Sender's name, edit this
-sender_email = "Sender@gmail.com"    # Sender's email address, edit this
-sender_password = "sender password" # Sender's password, edit this
+sender_name = "Word Point Ministries" # Sender's name, edit this
+sender_email = "wordpointm@gmail.com"    # Sender's email address, edit this
+sender_password = "zqrq ixzj jyao lngw " # Sender's password, edit this
+
+# sender_name = "Ogundare Olamide Emmanuel"
+# sender_email = "ogundareolamideemmanuel@gmail.com"
+# sender_password = "zwlm mvlb gvsd irzj"
