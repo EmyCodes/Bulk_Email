5a0b76828fed7f8d039e5ba4be1c3de332458606
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-26 17:14:20 +0100

    fix: resolve JSONDecoderError

--- resources/fit_camp_23.py
+++ resources/fit_camp_23.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import json
 import os
 
 participants = [
@@ -14,4 +13,4 @@ subject = "Reminder: Check-In Today at 6:00PM for FIT CAMP 2023"
 file_path = os.path.join(os.path.dirname(__file__), "fit_camp_23.html")
 
 with open(file_path, mode="r", encoding="utf-8") as f:
-    body = json.load(f)
+    body = f.read()
