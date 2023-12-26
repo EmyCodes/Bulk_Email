3995d7dee0c681e0e10e4b089d34f623bd1ff1b5
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-26 18:02:43 +0100

    fix: resolve circular error

--- send.py
+++ send.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-from models import send_email
+from models.sender import send_email
 from resources import participants, subject, body
 
-
-smtp_server = "smtp.gmail.com" # This is for Gmail, It can be changed to any other SMTP server
-
-sender_email = "sender@gmail.com"
-sender_password = "sender_password"
 
 for participant in participants:
     personalized_body = body.replace("[Participant's Name]", participant["name"].split()[1])
