3ba11e3c9c03aa9b8bdeaf8ab70ff5098169f833
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-26 18:11:15 +0100

    chore: comment

--- editable.py
+++ editable.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-smtp_server = "smtp.gmail.com" # This is for Gmail, It can be changed to any other SMTP server
+smtp_server = "smtp.gmail.com"  # This is for Gmail, It can be changed to any other SMTP server
 
-sender_email = "sender@mail.com"
-sender_password = "sender password"
+sender_email = "sender@mail.com"    # Sender's email address, edit this
+sender_password = "sender password" # Sender's password, edit this
