e0204debb115266abc233b6c9ba97ad615e5b5e1
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-26 22:23:20 +0100

    feat: handles error

--- models/sender.py
+++ models/sender.py
@@ -29,7 +29,12 @@ def send_email(recipent_email, subject, body):
     msg.attach(MIMEText(body, "html"))
     
     # Connect to the SMTP server
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-        server.login(sender_email, sender_password)
-        server.sendmail(sender_email, recipent_email, msg.as_string())
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, recipent_email, msg.as_string())
+            print("Successfully sent email to", recipent_email)
+    except Exception as e:
+        print(e)
+        print("Failed to send email to", recipent_email)
