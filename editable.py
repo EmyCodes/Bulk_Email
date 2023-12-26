9f57d6421319e58a5e9b78e7b8c24602d68e4e98
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-27 00:45:19 +0100

    mode: change mode

--- editable.py
+++ editable.py
@@ -4,7 +4,7 @@ smtp_server = "smtp.gmail.com"  # This is for Gmail, It can be changed to any ot
 
 sender_name = "Word Point Ministries" # Sender's name, edit this
 sender_email = "wordpointm@gmail.com"    # Sender's email address, edit this
-sender_password = "zqrq ixzj jyao lngw " # Sender's password, edit this
+sender_password = "oemp debw gxut uott" # Sender's password, edit this
 
 # sender_name = "Ogundare Olamide Emmanuel"
 # sender_email = "ogundareolamideemmanuel@gmail.com"
