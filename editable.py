3781ac297b105df2012054179bf2bc9790eb9535
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-26 22:25:53 +0100

    chore: update with sender name

--- editable.py
+++ editable.py
@@ -2,6 +2,6 @@
 
 smtp_server = "smtp.gmail.com"  # This is for Gmail, It can be changed to any other SMTP server
 
-sender_name = "Sender Name" # Sender's name, edit this
-sender_email = "sender@gmail.com"    # Sender's email address, edit this
-sender_password = "Sender Password " # Sender's password, edit this
+sender_name = "Sender's Name"   # Sender's name, edit this
+sender_email = "Sender@gmail.com"    # Sender's email address, edit this
+sender_password = "sender password" # Sender's password, edit this
