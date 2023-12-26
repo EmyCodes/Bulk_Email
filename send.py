5e4d8ac8cc5159a04138d0ee955b8e5fad717b29
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-26 17:48:52 +0100

    feat: add placeholder for sender email, sender password and smtp server

--- send.py
+++ send.py
@@ -2,8 +2,14 @@
 from models import send_email
 from resources import participants, subject, body
 
+
+smtp_server = "smtp.gmail.com" # This is for Gmail, It can be changed to any other SMTP server
+
+sender_email = "sender@gmail.com"
+sender_password = "sender_password"
+
 for participant in participants:
-    personalized_body = body.replace("[Participant's Name]", participant["name"])
+    personalized_body = body.replace("[Participant's Name]", participant["name"].split()[1])
 
     send_email(participant["email"], subject, personalized_body)
 
