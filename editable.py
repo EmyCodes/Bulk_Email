3267d0345343dc24acfaead1edfb2e82ea5459dc
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-26 18:04:00 +0100

    feat: sender details

---
+++ editable.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+smtp_server = "smtp.gmail.com" # This is for Gmail, It can be changed to any other SMTP server
+
+sender_email = "sender@mail.com"
+sender_password = "sender password"
