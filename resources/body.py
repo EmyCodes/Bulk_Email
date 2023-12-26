ef60409eebaabcc348f7082bcb732f6f72f25d5d
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-26 18:07:55 +0100

    rename: change file name

--- resources/body.py
+++ resources/body.py
@@ -11,7 +11,7 @@ participants = [
 subject = "Reminder: Check-In Today at 6:00PM for FIT CAMP 2023"
 
 
-file_path = os.path.join(os.path.dirname(__file__), "fit_camp_23.html")
+file_path = os.path.join(os.path.dirname(__file__), "body.html")
 
 with open(file_path, mode="r", encoding="utf-8") as f:
     body = f.read()
