ca9ed088597f2f73e80da89f4b8f608c0caf8207
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-27 10:04:10 +0100

    chore: update slightly

--- send.py
+++ send.py
@@ -7,3 +7,7 @@ print("Sending emails...")
 for index, participant in participants.iterrows():
     personalized_body = body.replace("[Participant's Name]", participant["Name"].split()[1])
     send_email(participant["Email"], subject, personalized_body)
+
+# for participant in participants:
+#     personalized_body = body.replace("[Participant's Name]", participant["name"].split()[1])
+#     send_email(participant["email"], subject, personalized_body)
